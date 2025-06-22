ba86f3ad95b285ecdde347633af7d18d03ff7104
Seb <hornse@arcor.de>
2025-06-22 15:20:37 +0200

    data.php angepasst

--- data.php
+++ data.php
@@ -1,5 +1,16 @@
 <?php
-$db = new PDO('sqlite:db.sqlite');
-$stmt = $db->query("SELECT klasse, SUM(kilometer) AS kilometer FROM eintraege GROUP BY klasse ORDER BY kilometer DESC");
-echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
-?>
+header('Content-Type: application/json');
+
+$db = new PDO('sqlite:../db.sqlite');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$result = $db->query('SELECT klasse, SUM(km) as km FROM eintraege GROUP BY klasse ORDER BY km DESC');
+$klassen = $result->fetchAll(PDO::FETCH_ASSOC);
+
+include 'farben.php';
+
+foreach ($klassen as &$eintrag) {
+    $eintrag['farbe'] = $farben[$eintrag['klasse']] ?? '#888888';
+}
+
+echo json_encode($klassen);
